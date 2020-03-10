695eb16e9446294c096a43cd386dd8ca9fbbc5d5
ashraf <ashraf@machbtech.com>
2020-03-10 17:46:47 -0500

    experimenting with react

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,44 +1,27 @@
 import { addExpandCollapseBehavior } from "./js-utils.js";
-import UniversalRouter from '/web_modules/universal-router.js'
+import Navigo from "/web_modules/navigo.js";
 
-addExpandCollapseBehavior(document.getElementById("main-menu-button"),  {
-    ".grid-container": ["collapsed"]
+/*
+Routing
+ */
+
+ const router = new Navigo();
+ router.on({
+    '/': function () {
+      console.log('base');
+    },
+    'manage-user': function () {
+       console.log('manage-user');
+    },
+    'content': function () {
+      console.log('content');
+    },
+    '*': function () {
+      console.log('*');
+    }
+  }).resolve();
+
+addExpandCollapseBehavior(document.getElementById("main-menu-button"), {
+  ".grid-container": ["collapsed"]
 });
 
-
-const routes = [
-  {
-    path: '/manage-user',
-    action: () => console.log('checking child routes for /posts'),
-    children: [
-      {
-        path: '', // optional, matches both "/posts" and "/posts/"
-        action: () => `<h1>test</h1>`
-      },
-      {
-        path: '/:id',
-        action: (context) => `<h1>Post #${context.params.id}</h1>`
-      }
-    ]
-  }
-];
-const routePaths = routes.map(route => route.path)
-const router = new UniversalRouter(routes); 
-
-document.getElementById("main-menu").addEventListener("click", (e) => {
-    if(!e.target.href) {
-      return;
-    }
-    const path = (new URL(e.target.href)).pathname
-    if(routePaths.indexOf((new URL(e.target.href)).pathname != -1)){
-       e.preventDefault();
-       const { title=''  } = e.target;
-       const state = { 'page_id': 1, 'user_id': 5 };
-       history.pushState(state, title, path)
-    }
-});
-
-
-// router.resolve('/manage-user').then(html => {
-//   document.getElementsByTagName('main')[0].html = html // renders: <h1>Posts</h1>
-// })
--- src/js/toggle-button.js
+++ src/js/toggle-button.js
@@ -1,29 +1,37 @@
 class ToggleButton extends HTMLButtonElement {
-    connectedCallback() {
-        this.setAttribute("aria-pressed", "false");
-        this.addEventListener("click", this.togglePressed);
-        this.render();
-    }
+  connectedCallback() {
+    this.setAttribute("aria-pressed", "false");
+    this.addEventListener("click", this.togglePressed);
+    this.render();
+  }
 
-    togglePressed() {
-        const isPressed = this.getAttribute("aria-pressed") === "true";
-        this.setAttribute("aria-pressed", `${!isPressed}`);
-        this.render();
-    }
+  togglePressed() {
+    const isPressed = this.getAttribute("aria-pressed") === "true";
+    this.setAttribute("aria-pressed", `${!isPressed}`);
+    this.render();
+  }
 
-    render() {
-        const isPressed = this.getAttribute("aria-pressed") === "true";
-        const buttonText =  isPressed ? this.getAttribute("pressed-text") : this.getAttribute("unpressed-text");
-        let icon = "";
-        if(this.getAttribute("pressed-icon") ){
-            icon =  `<span aria-hidden="true" class=${this.getAttribute("icon-family")}>${isPressed ? this.getAttribute("pressed-icon") : this.getAttribute("unpressed-icon")}</span> `;
-        }
-        this.innerHTML = `<span class=${this.getAttribute("class-names")}>
+  render() {
+    const isPressed = this.getAttribute("aria-pressed") === "true";
+    const buttonText = isPressed
+      ? this.getAttribute("pressed-text")
+      : this.getAttribute("unpressed-text");
+    let icon = "";
+    if (this.getAttribute("pressed-icon")) {
+      icon = `<span aria-hidden="true" class=${this.getAttribute(
+        "icon-family"
+      )}>${
+        isPressed
+          ? this.getAttribute("pressed-icon")
+          : this.getAttribute("unpressed-icon")
+      }</span> `;
+    }
+    this.innerHTML = `<span class=${this.getAttribute("class-names")}>
                              <span class="button-text">${buttonText}</span>
                              ${icon}
                           </span>`;
-        console.log(this.innerHTML);
-    }
+    console.log(this.innerHTML);
+  }
 }
 
-customElements.define("toggle-button", ToggleButton, {extends: "button"});
+customElements.define("toggle-button", ToggleButton, { extends: "button" });
